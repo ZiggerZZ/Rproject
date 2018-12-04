25849ca90606955038905edee03f7adab345857d
ZiggerZZ <ziggerZZ@gmail.com>
2018-12-04 18:20:32 +0100

    Package version 0.0.0.9001
    The problem of data is solved.
    Empty vignette is added.

--- trainpack/R/fonction_temps_moyen.R
+++ trainpack/R/fonction_temps_moyen.R
@@ -9,13 +9,9 @@
 #' @export
 #'
 #' @examples fonction_temps_moyen(gareA = "METZ", gareB = "PARIS EST")
-fonction_temps_moyen <- function(gareA, gareB){
-  SNCF_regularite_modif <-
-    SNCF_regularite %>%
-    filter("Gare de départ"==gareA, "Gare d'arrivée"==gareB)
-
-  SNCF_regularite_modif %>%
-    select("Durée moyenne du trajet (min)") %>%
-    mutate("Durée moyenne du trajet (min)" = as.numeric(as.character("Durée moyenne du trajet (min)"))) %>%
-    summarise(Mean_Time = mean(as.numeric("Durée moyenne du trajet (min)")))
+fonction_temps_moyen <- function(gareA, gareB) {
+  SNCF_regularite %>%
+    filter(`Gare de départ` == gareA, `Gare d'arrivée` == gareB) %>%
+    select(`Durée moyenne du trajet (min)`) %>%
+    summarise(Mean_Time = mean(`Durée moyenne du trajet (min)`))
 }
--- trainpack/data-raw/modify_data.R
+++ trainpack/data-raw/modify_data.R
@@ -1,3 +1,9 @@
 library(readr)
-SNCF_regularite <- read_csv2("data-raw/regularite-mensuelle-tgv-aqst.csv") #normally there shouldn't be parent directory data-raw/
-usethis::use_data(SNCF_regularite, overwrite = TRUE)
+library(dplyr)
+
+SNCF_regularite <- read_delim("data-raw/regularite-mensuelle-tgv-aqst.csv", delim = ";", locale = locale(decimal_mark = ".")) #grouping_mark = ","
+SNCF_regularite <- SNCF_regularite %>% mutate_at(vars(`Retard pour causes externes`,`Retard à cause infrastructure ferroviaire`,
+                                                      `Retard à cause gestion trafic`, `Retard à cause matériel roulant`,
+                                                      `Retard à cause gestion en gare et réutilisation de matériel`,
+                                                      `Retard à cause prise en compte voyageurs`), function(x) x/100)
+usethis::use_data(SNCF_regularite, overwrite = TRUE, internal = TRUE)
