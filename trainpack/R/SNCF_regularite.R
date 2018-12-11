de794bc2b23406051c36d686745f58f1c787576d
ZiggerZZ <ziggerZZ@gmail.com>
2018-12-11 17:58:41 +0100

    License and data documentation added

---
+++ trainpack/R/SNCF_regularite.R
@@ -0,0 +1,12 @@
+#' Information about the SNCF trains starting from 2015.
+#'
+#' A dataset containing the statistics and other information about the delays of SNCF trains.
+#'
+#' @format A data frame with 5032 rows and 34 variables:
+#' \describe{
+#'   \item{annee}{year}
+#'   \item{mois}{month}
+#'   ...
+#' }
+#' @source \url{https://data.sncf.com/explore/dataset/regularite-mensuelle-tgv-aqst/table/?sort=periode&fbclid=IwAR1iEGCOzJKKqUCVA-zJLW8-3w9zKi1R7LhYYd5BsBVlqvCCZvyafZlUqN4}
+"SNCF_regularite"
---
+++ trainpack/R/lemonde_dates.fr.R
@@ -0,0 +1,12 @@
+#' Information about the article of Le Monde.
+#'
+#' A dataset containing the information about articles of Le Monde containing words "SNCF" and "grève".
+#'
+#' @format A data frame with 5032 rows and 34 variables:
+#' \describe{
+#'   \item{annee}{year}
+#'   \item{mois}{month}
+#'   \item{n()}{count}
+#' }
+#' @source \url{https://www.lemonde.fr/}
+"lemonde_dates"
