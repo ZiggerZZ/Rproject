10e39352038eaa31d6c4d756bcc26afe7f8dc8a3
xavieradbda <xavier.de-boisredon@hec.edu>
2018-12-13 14:44:42 +0100

    Update cote_tbl.R

--- trainpack/R/cote_tbl.R
+++ trainpack/R/cote_tbl.R
@@ -4,20 +4,20 @@
 #'
 #' @format A data frame with 123 rows and 14 variables:
 #' \describe{
-#'   \item{gare_de_depart}{department code}
-#'   \item{gare_d_arrivee}{region name}
-#'   \item{lon_depart}{shape of the department France in crs 2154}
-#'   \item{lat_depart}{region name}
-#'   \item{lon_arrivee}{shape of the department France in crs 2154}
-#'   \item{lat_arrivee}{region name}
-#'   \item{postcode_depart}{shape of the department France in crs 2154}
-#'   \item{postcode_arrivee}{region name}
-#'   \item{country_depart}{shape of the department France in crs 2154}
-#'   \item{country_arrivee}{region name}
-#'   \item{CODE_DEPT_depart}{shape of the department France in crs 2154}
-#'   \item{CODE_DEPT_arrivee}{region name}
-#'   \item{CODE_DEPT}{shape of the department France in crs 2154}
-#'   \item{cote}{region name}
+#'   \item{gare_de_depart}{departure trainstation}
+#'   \item{gare_d_arrivee}{arrival trainstation}
+#'   \item{lon_depart}{longitude of departure}
+#'   \item{lat_depart}{latitude of departure}
+#'   \item{lon_arrivee}{longitude of arrival}
+#'   \item{lat_arrivee}{latitude of arrival}
+#'   \item{postcode_depart}{postcode}
+#'   \item{postcode_arrivee}{postcode arrival}
+#'   \item{country_depart}{country of departure}
+#'   \item{country_arrivee}{country of arrival}
+#'   \item{CODE_DEPT_depart}{department code departure}
+#'   \item{CODE_DEPT_arrivee}{department code arrival}
+#'   \item{CODE_DEPT}{department code}
+#'   \item{cote}{cote of train line}
 #'   }
 #' @source {google maps api + scnf_regularite}
 "cote_tbl"
