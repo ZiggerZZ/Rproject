7577fe7d9545c71fa28226cae5f2b208078220de
xavieradbda <xavier.de-boisredon@hec.edu>
2018-12-13 14:29:37 +0100

    added department data description file

---
+++ trainpack/R/departement.R
@@ -0,0 +1,11 @@
+#' French departements and regions file
+#'
+#' A dataset containing shapefile for France's departments and region.
+#'
+#' @format A data frame with 5032 rows and 34 variables:
+#' \describe{
+#'   \item{CODE_DEPT}{department code}
+#'   \item{NOM_REG}{region name}
+#'   \item{geometry}{shape of the department France in crs 2154}
+#'   }
+#' @source \url{https://git.thinkr.fr/xhec/2018/tree/master/assignment/assignment%201/data/departements}
