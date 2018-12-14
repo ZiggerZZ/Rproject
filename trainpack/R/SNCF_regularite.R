a9fff8a7fc09db3294c21bdcd31e43492a473028
alexandre-minydetornaco <44435998+alexandre-minydetornaco@users.noreply.github.com>
2018-12-14 18:33:47 +0100

    Add files via upload
    English translation

--- trainpack/R/SNCF_regularite.R
+++ trainpack/R/SNCF_regularite.R
@@ -20,25 +20,24 @@
 #'   \item{nombre_de_trains_en_retard_a_l_arrivee}{Number of trains late at arrival}
 #'   \item{retard_moyen_des_trains_en_retard_a_l_arrivee}{Average delay of trains late at arrival (min)}
 #'   \item{retard_moyen_de_tous_les_trains_a_l_arrivee}{Average delay of all the trains late at arrival (min)}
-#'   \item{commentaire_facultatif_retards_a_l_arrivee}{Comment about delays at arrival}
-#'   TRANSLATE TO ENGLISH THE FOLLOWING DESCRIPTIONS
-#'   \item{trains_en_retard_pour_causes_externes}{\% trains en retard pour causes externes (météo, obstacles, colis suspects, malveillance, mouvements sociaux, etc.)}
-#'   \item{trains_en_retard_a_cause_infrastructure_ferroviaire}{\% trains en retard à cause infrastructure ferroviaire (maintenance, travaux)}
-#'   \item{trains_en_retard_a_cause_gestion_trafic}{\% trains en retard à cause gestion trafic (circulation sur ligne ferroviaire, interactions réseaux)}
-#'   \item{trains_en_retard_a_cause_materiel_roulant}{\% trains en retard à cause matériel roulant}
-#'   \item{trains_en_retard_a_cause_gestion_en_gare_et_reutilisation_de_materiel}{\% trains en retard à cause gestion en gare et réutilisation de matériel}
-#'   \item{trains_en_retard_a_cause_prise_en_compte_voyageurs}{\% trains en retard à cause prise en compte voyageurs (affluence, gestions PSH, correspondances)}
-#'   \item{nombre_trains_en_retard_15min}{Nombre trains en retard > 15min (si liaison concurrencée par vol)}
-#'   \item{retard_moyen_trains_en_retard_15min}{Retard moyen trains en retard > 15min (si liaison concurrencée par vol)}
-#'   \item{nombre_trains_en_retard_30min}{Nombre trains en retard > 30min}
-#'   \item{nombre_trains_en_retard_60min}{Nombre trains en retard > 60min}
-#'   \item{periode}{Period (date)}
-#'   \item{retard_pour_causes_externes}{Retard pour causes externes (météo, obstacles, colis suspects, malveillance, mouvements sociaux, etc.)}
-#'   \item{retard_a_cause_infrastructure_ferroviaire}{Retard à cause infrastructure ferroviaire (maintenance, travaux)}
-#'   \item{retard_a_cause_gestion_trafic}{Retard à cause gestion trafic (circulation sur ligne ferroviaire, interactions réseaux)}
-#'   \item{retard_a_cause_materiel_roulant}{Retard à cause matériel roulant}
-#'   \item{retard_a_cause_gestion_en_gare_et_reutilisation_de_materiel}{Retard à cause gestion en gare et réutilisation de matériel}
-#'   \item{retard_a_cause_prise_en_compte_voyageurs}{Retard à cause prise en compte voyageurs (affluence, gestions PSH, correspondances)}
+#'   \item{commentaire_facultatif_retards_a_l_arrivee}{Comments about delays at arrival}
+#'   \item{trains_en_retard_pour_causes_externes}{\% of trains delayed because of external causes (weather, obstacles, suspects package, strikes, etc.)}
+#'   \item{trains_en_retard_a_cause_infrastructure_ferroviaire}{\% of trains delayed because of railway infrastructure (maintenance, works)}
+#'   \item{trains_en_retard_a_cause_gestion_trafic}{\% of trains delayed because of traffic management}
+#'   \item{trains_en_retard_a_cause_materiel_roulant}{\% of trains delayed because of rolling materials}
+#'   \item{trains_en_retard_a_cause_gestion_en_gare_et_reutilisation_de_materiel}{\% of trains delayed because of station management and material reusal}
+#'   \item{trains_en_retard_a_cause_prise_en_compte_voyageurs}{\% of trains delayed because of travelers (peak hours, correspondences)}
+#'   \item{nombre_trains_en_retard_15min}{Number of train delayed > 15min}
+#'   \item{retard_moyen_trains_en_retard_15min}{Average delay of train delayed more than 15min}
+#'   \item{nombre_trains_en_retard_30min}{Number of train delayed > 30min}
+#'   \item{nombre_trains_en_retard_60min}{Number of train delayed > 60min}
+#'   \item{periode}{Period }
+#'   \item{retard_pour_causes_externes}{Delays because of external causes (weather, obstacles, suspects package, strikes, etc.)}
+#'   \item{retard_a_cause_infrastructure_ferroviaire}{Delays because of railway infrastructure (maintenance, works)}
+#'   \item{retard_a_cause_gestion_trafic}{Delays because of traffic management}
+#'   \item{retard_a_cause_materiel_roulant}{Delays because of rolling materials}
+#'   \item{retard_a_cause_gestion_en_gare_et_reutilisation_de_materiel}{Delays because of station management and material reusal}
+#'   \item{retard_a_cause_prise_en_compte_voyageurs}{Delays because of travelers (peak hours, correspondences)}
 #' }
 #' @source \url{https://data.sncf.com/explore/dataset/regularite-mensuelle-tgv-aqst/table/?sort=periode&fbclid=IwAR1iEGCOzJKKqUCVA-zJLW8-3w9zKi1R7LhYYd5BsBVlqvCCZvyafZlUqN4}
 "SNCF_regularite"
