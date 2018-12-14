93caad4503c85da665396a6c232d5a1908c58e8e
ZiggerZZ <ziggerZZ@gmail.com>
2018-12-14 18:27:07 +0100

    Final dependencies.R

--- trainpack/inst/dependencies.R
+++ trainpack/inst/dependencies.R
@@ -1,6 +1,6 @@
 # No Remotes ----
 # Attachments ----
-to_install <- c("dplyr", "jsonlite", "magick", "sf", "stats")
+to_install <- c("dplyr", "DT", "ggplot2", "jsonlite", "magick", "sf", "shiny", "shinydashboard", "stats", "tidyr", "tidyverse")
   for (i in to_install) {
     message(paste("looking for ", i))
     if (!requireNamespace(i)) {
