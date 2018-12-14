4916c303d9732b9676231a35ab62419979d1dc7f
tristanmayer <44436786+tristanmayer@users.noreply.github.com>
2018-12-14 13:39:06 +0100

    Update fonction_article.R
    change month

--- trainpack/R/fonction_article.R
+++ trainpack/R/fonction_article.R
@@ -12,20 +12,20 @@
 
 fonction_article <- function(mois){
   trainpack::lemonde_dates %>% mutate(month = case_when(
-    month == "janvier" ~ 1,
-    month == "février" ~ 2,
-    month == "mars" ~ 3,
-    month == "avril" ~ 4,
-    month == "mai" ~ 5,
-    month == "juin" ~ 6,
-    month == "juillet" ~ 7,
-    month == "août" ~ 8,
-    month == "septembre" ~ 9,
-    month == "octobre" ~ 10,
-    month == "novembre" ~ 11, 
-    month == "décembre" ~ 12
+    month == "janvier" ~ "Jan",
+    month == "février" ~ "Fev",
+    month == "mars" ~ "Mar",
+    month == "avril" ~ "Apr",
+    month == "mai" ~ "May",
+    month == "juin" ~ "Jun",
+    month == "juillet" ~ "Jul",
+    month == "août" ~ "Aug",
+    month == "septembre" ~ "Sep",
+    month == "octobre" ~ "Oct",
+    month == "novembre" ~ "Nov", 
+    month == "décembre" ~ "Dec"
   )) %>%
     filter(month == mois) %>%
     summarise(nb_of_article = mean(n)) %>% 
     as.numeric()
-  }
+}
